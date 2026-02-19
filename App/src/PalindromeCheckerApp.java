be48e039562d5647c6469434676fee8f071f460c
jl8629 <jl8629@srmist.edu.in>
2026-02-19 10:17:34 +0530

    Gayathri added UC1-WelcomePage

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,33 @@
+//version 1.0
+//author Jasmitha
+//useCase 1 : Welcome Page
+
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+
+        /*
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String input = sc.nextLine();
+
+        String reversed = "";
+
+        for(int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        if(input.equals(reversed)) {
+            System.out.println("It is a palindrome");
+        } else {
+            System.out.println("It is not a palindrome");
+        }
+        sc.close();
+         */
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PalindroneCheckerApp
+
+
+A basic welcome page was created for the Palindrome Checker System to show the version of the system and to show that the system has been initialized 
